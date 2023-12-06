80408522ff3f6b82fb7c6cfe631eac10597d42c2
sg-educate <wajidfareed103@gmail.com>
2023-12-06 22:32:40 +0500

    home page fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,10 +1,9 @@
-<?php
-
+<?php 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -14,6 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            // Initial columns from the first migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -21,8 +21,44 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            // Additional columns from the second migration
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 32)->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('phone_number', 32)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('state', 50)->nullable();
+            $table->string('zip', 11)->nullable();
+            $table->string('avatar')->default('uploads/d-avatar.jpg');
+            $table->string('cover')->default('uploads/d-cover.jpg');
+            $table->string('website', 50)->nullable();
+            $table->string('twitter', 50)->nullable();
+            $table->string('linkedin', 32)->nullable();
+            $table->string('youtube', 100)->nullable();
+            $table->enum('follow_privacy', ['public', 'private'])->default('public');
+            $table->string('post_privacy')->default('ifollow');
+            $table->boolean('showlastseen')->default(true);
+            $table->boolean('emailNotification')->default(true);
+            $table->text('about')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
+            $table->enum('type', ['user', 'admin'])->default('user'); // Updated from the third migration
+            $table->string('subscription_plan', 100)->default('free'); // Added from the third migration
+            $table->json('details')->nullable();
+            $table->unsignedInteger('referrer_id')->nullable();
+            $table->timestamp('last_data_update')->nullable();
+            $table->unsignedInteger('user_id')->nullable(); // For referencing old DB
         });
-        User::create(['name' => 'Admin','email' => 'admin@themesbrand.com','password' => Hash::make('12345678'),'email_verified_at'=> now(), 'created_at' => now(),]);
+
+        // Creating a default admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@richtv.io',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+            'created_at' => now()
+        ]);
     }
 
     /**
