bc19b5da9d6750721d5bcd16f77249f85b348e5a
M-usama-khn <usamasg7@gmail.com>
2023-12-12 22:44:02 +0500

    remove heading on exam page

--- resources/views/exams/index.blade.php
+++ resources/views/exams/index.blade.php
@@ -4,16 +4,6 @@
   @endsection
   @section('content')
   <section class="container-lg mt-3 py-80">
-    <div class="exam-header text-center bg-white shadow-sm p-4">
-      <h2 class="fw-6 text-uppercase m-0">Watch this Space</h2>
-      <div class="border border-bottom border-primary d-inline-block mb-2" style="width: 74px;"></div>
-      <p class="mb-0">The ever-evolving world of trading demands you to upgrade your investing skillset
-        regularly.
-        That’s why we will keep adding fresh exams here for you on a frequent basis. Don’t forget to stop by
-        the
-        exam center from time to time.</p>
-    </div>
-
     <div class="my-3 exam-cards mt-5 pb-4">
       <div class=" mb-4">
         <h2 class="fw-6 text-uppercase m-0">FUNDAMENTAL Exams</h2>
