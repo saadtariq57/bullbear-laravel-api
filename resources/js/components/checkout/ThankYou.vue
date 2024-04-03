349e4013d30a233dd3abf37960849915e5da763b
Wajid-Fareed <wajidfareed103@gmail.com>
2024-04-03 22:33:14 +0500

    checkout css fix

---
+++ resources/js/components/checkout/ThankYou.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center my-5">
+        <h1>You have successfully subscribed!</h1>
+        <p>Thank you for choosing our service. Your support means a lot to us.</p>
+        <!-- <p>RichTV is dedicated to providing high-quality content that educates, entertains, and empowers viewers
+            interested in finance, investment, and technology. Stay tuned for insightful market analyses, expert
+            interviews, and informative discussions aimed at helping you make informed decisions in the world of
+            finance.</p> -->
+        <img src="
+        /build/images/thank-you.png" alt="">
+    </div>
+</template>
--- resources/js/components/checkout/checkout.vue
+++ resources/js/components/checkout/checkout.vue
@@ -1,33 +1,43 @@
 <template>
     <section class="container-fluid my-4">
-        <div class="row">
-            <h1 class="fw-bold fs-1 text-uppercase">Checkout</h1>
-            <div class="border-heading d-inline-block mt-4 mb-5"></div>
+        <div class="row justify-content-center">
+            <h1 class="fw-bold fs-1 text-uppercase px-0 text-center">Checkout</h1>
+            <div class="border-heading d-inline-block mt-1 mb-5"></div>
         </div>
         <div class="row">
-            <h2 v-if="userSelectedPlan">You have Selected {{ userSelectedPlan.name }} Plan</h2>
-            <h3 v-if="userSelectedPlan">Plan price CAD {{ planType ? userSelectedPlan.yearly_price : userSelectedPlan.monthly_price}} </h3>
-            <!-- Subscription Form -->
-            <form @submit.prevent="handleSubmit" id="subscription-form">
-                <div>
-                    <label>Enter Card Holder Name</label>
-                    <input type="text" v-model="formData.cardHolderName" id="cardHolderName" name="cardHolderName" class="form-control" placeholder="Card holder name">
+            <div class="checkout-wrapper border py-2 shadow-sm">
+                <div class="checkout-summary">
+                    <!-- <h2 v-if="userSelectedPlan" class="fw-5 fs-3">You have Selected {{ userSelectedPlan.name }} Plan</h2> -->
+                    <!-- <h3 v-if="userSelectedPlan">Plan price CAD {{ planType ? userSelectedPlan.yearly_price : userSelectedPlan.monthly_price}} </h3> -->
                 </div>
-                <div>
-                    <label>plan name</label>
-                    <input type="text" v-model="formData.planName" id="planName" name="planName" class="form-control"  disabled>
-                </div>
-                <!-- <input type="text" value="{{ showYearly ? selectedPlan.yearly_price : selectedPlan.monthly_price}}" name="price_intent" id="price_intent"> -->
-                <!-- Payment Method Element -->
-                <label for="card-element">Credit or Debit Card</label>
-                <div id="card-element">
-                    <!-- A Stripe Element will be inserted here. -->
-                </div>
+                <!-- Subscription Form -->
+                <form @submit.prevent="handleSubmit" id="subscription-form">
+                    <div class="mb-2">
+                        <label for="planName" class="form-label">Plan Name</label>
+                        <input type="text" v-model="formData.planName" id="planName" name="planName"
+                            class="form-control border shadow-sm" disabled>
+                    </div>
+                    <div class="mb-2" v-if="userSelectedPlan">
+                        <label for="planPrice" class="form-label">Plan Price</label>
+                        <input type="text" id="planPrice" name="planPrice" class="form-control border shadow-sm"
+                            :value="planType ? userSelectedPlan.yearly_price : userSelectedPlan.monthly_price" disabled>
+                    </div>
+                    <div class="mb-2">
+                        <label for="cardHolderName" class="form-label">Enter Card Holder Name</label>
+                        <input type="text" v-model="formData.cardHolderName" id="cardHolderName" name="cardHolderName"
+                            class="form-control border shadow-sm" placeholder="Card holder name">
+                    </div>
+                    <!-- <input type="text" value="{{ showYearly ? selectedPlan.yearly_price : selectedPlan.monthly_price}}" name="price_intent" id="price_intent"> -->
+                    <!-- Payment Method Element -->
+                    <label for="card-element">Credit or Debit Card</label>
+                    <div id="card-element">
+                        <!-- A Stripe Element will be inserted here. -->
+                    </div>
 
-                <!-- Form submission -->
-                <button type="submit" id="submit-button" class="btn btn-primary mt-4">Subscribe</button>
-            </form>
-            
+                    <!-- Form submission -->
+                    <button type="submit" id="submit-button" class="btn btn-primary mt-4 w-100" :disabled="submitform">Subscribe</button>
+                </form>
+            </div>
         </div>
     </section>
 </template>
@@ -38,7 +48,7 @@ import { mapState, mapActions } from 'vuex';
 export default {
     name: 'PricingComponent',
     computed: {
-    ...mapState('userSubscriptionModule', ['selectedPlan', 'showYearly']),
+        ...mapState('userSubscriptionModule', ['selectedPlan', 'showYearly']),
     },
     data() {
         return {
@@ -50,7 +60,8 @@ export default {
             },
             storedPlan: null,
             userSelectedPlan: null,
-            planType: true
+            planType: true,
+            submitform: false,
         };
     },
     mounted() {
@@ -60,7 +71,7 @@ export default {
         // console.log('yearly plan: '+this.showYearly);
     },
     methods: {
-        getPlan(){
+        getPlan() {
             this.storedPlan = localStorage.getItem('selectedPlan');
             if (this.storedPlan) {
                 this.userSelectedPlan = JSON.parse(this.storedPlan);
@@ -76,7 +87,7 @@ export default {
             if (this.planType) {
                 console.log('checkout:', this.planType);
             } else {
-                this.planType = this.showYearly; 
+                this.planType = this.showYearly;
             }
 
         },
@@ -85,16 +96,16 @@ export default {
             var planPriceInput = document.createElement('input');
             planPriceInput.setAttribute('type', 'hidden');
             console.log('price intent created');
-            if(this.planType){
+            if (this.planType) {
                 var priceIntent = this.userSelectedPlan.stripe_yearly_price_id;
-            }else{
+            } else {
                 var priceIntent = this.userSelectedPlan.stripe_monthly_price_id;
             }
             planPriceInput.setAttribute('name', 'price_Intent');
             planPriceInput.setAttribute('value', priceIntent);
             form.appendChild(planPriceInput);
             this.formData.price_Intent = priceIntent;
-            
+
         },
         stripePaymentMethod() {
             var stripePromise = loadStripe('pk_test_51Hwe7XJYG6q3yq60zNuah9X3DSCMF4142Y43Ufsz2KlZJpz1cLTKzrMkUMlFB3OATwluVbqWdzqrp2unJOAUt5Gg00EipuAVQa');
@@ -103,7 +114,11 @@ export default {
                 var elements = stripe.elements();
                 var card = elements.create('card');
                 card.mount('#card-element');
-
+                var cardElementContainer = document.getElementById('card-element');
+                cardElementContainer.style.border = '1px solid #ddd7d7';
+                cardElementContainer.style.padding = '10px';
+                cardElementContainer.style.borderRadius = '4px';
+                cardElementContainer.style.boxShadow = '0 1px 1px rgba(0, 0, 0, .05)';
                 var form = document.getElementById('subscription-form');
                 form.addEventListener('submit', async (event) => {
                     event.preventDefault();
@@ -129,18 +144,23 @@ export default {
             paymentMethodInput.setAttribute('value', paymentMethodId);
             form.appendChild(paymentMethodInput);
             this.formData.payment_method = paymentMethodId;
+            this.submitform = true;
             if (!paymentMethodId) {
                 console.error('Invalid payment method ID');
-            }else{
+            } else {
                 axios.post('/api/createUserSubscription', this.formData)
                     .then(response => {
                         // Handle response
-                        console.log('Response:', response.data);
+                        // console.log('Response:', response.data);
+                        this.$router.push('/checkout/thank-you');
+                        this.submitform = false;
                     })
-                .catch(error => {
-                    // Handle error
-                    console.error('Error:', error);
-                });
+                    .catch(error => {
+                        // Handle error
+                        console.error('Error:', error);
+                        this.submitform = false;
+
+                    });
             }
         },
         // handleSubmit() {
@@ -149,3 +169,22 @@ export default {
     },
 };
 </script>
+<style>
+.border {
+    border-color: #dfdfdf !important;
+}
+
+.checkout-wrapper {
+    max-width: 400px;
+    margin: 0 auto;
+}
+
+.CardField-input-wrapper {
+    overflow: visible;
+}
+
+.InputContainer .InputElement {
+    border: 1px solid #dfdfdf !important;
+    padding: 10px 5px;
+}
+</style>
