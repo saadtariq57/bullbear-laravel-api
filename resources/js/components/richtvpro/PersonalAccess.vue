9ea69ee2f7e4367d0954003f6f50c1b4238e2770
Wajid-Fareed <wajidfareed103@gmail.com>
2024-05-31 22:22:31 +0500

    Personal Access

--- resources/js/components/richtvpro/PersonalAccess.vue
+++ resources/js/components/richtvpro/PersonalAccess.vue
@@ -1,58 +1,92 @@
 <template>
     <div class="container my-4">
-    <div class="personal-head text-center border-bottom mb-4">
-        <h1 class="fs-2 fw-6">Personal Access</h1>
-    </div>
-    <div class="personal-card-wrapper row">
-        <div class="col-md-6">
-            <div class="card flex-fill shadow-sm border">
-                <div class="card-body text-center">
-                    <i class="bi bi-calendar3 fs-2"></i>
-                    <h2 class="fs-4 fw-5">Book An Appointment</h2>
-                    <p>Booking an appointment with a professional guarantees personalized assistance and efficient
-                        problem-solving.</p>
-                    <button class="btn btn-primary">Select</button>
+        <div class="personal-head text-center border-bottom mb-4">
+            <h1 class="fs-2 fw-6">Personal Access</h1>
+        </div>
+        <div class="personal-card-wrapper row py-4">
+            <div class="col-md-6">
+                <div class="card flex-fill shadow-sm border">
+                    <div class="card-body text-center">
+                        <i class="bi bi-calendar3 fs-2"></i>
+                        <h2 class="fs-4 fw-5">Book An Appointment</h2>
+                        <p>Booking an appointment with a professional guarantees personalized assistance and efficient
+                            problem-solving.</p>
+                        <button class="btn btn-primary" @click="showAppointmentModal()">Schedule</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card flex-fill shadow-sm border">
+                    <div class="card-body text-center">
+                        <i class="bi bi-chat-dots fs-2"></i>
+                        <h2 class="fs-4 fw-5">Chat With Professionals</h2>
+                        <p>Chatting with a professional provides convenient access to tailored expertise and guidance in
+                            real-time</p>
+                        <button class="btn btn-primary">Select</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="card flex-fill shadow-sm border">
-                <div class="card-body text-center">
-                    <i class="bi bi-chat-dots fs-2"></i>
-                    <h2 class="fs-4 fw-5">Chat With Professionals</h2>
-                    <p>Chatting with a professional provides convenient access to tailored expertise and guidance in
-                        real-time</p>
-                    <button class="btn btn-primary">Select</button>
+
+        <!-- Modal -->
+        <div class="modal fade" ref="appointment" id="appointmentModal" data-bs-backdrop="static"
+            data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Book an Appointment</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="hideAppointmentModal()"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Add Calendly inline widget embed code here -->
+                        <div class="calendly-inline-widget"
+                            data-url="https://calendly.com/wajidfareed103/book-an-appointment?hide_event_type_details=1&hide_gdpr_banner=1"
+                            style="min-width:320px; height:600px;"></div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="personal-form-wrapper d-flex justify-content-center mt-3">
-        <form class="w-50 border p-3 shadow-sm">
-            <div class="mb-3">
-                <label for="exampleInputName" class="form-label">Name</label>
-                <input type="text" class="form-control border shadow-sm" id="exampleInputName">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control border shadow-sm" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputDate" class="form-label">Date</label>
-                <input type="date" class="form-control border shadow-sm" id="exampleInputDate">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputTime" class="form-label">Time</label>
-                <input type="time" class="form-control border shadow-sm" id="exampleInputTime">
-            </div>
-            <button type="submit" class="btn btn-primary">Schedule</button>
-        </form>
-    </div>
-</div>
 </template>
+
+<script>
+import { Modal } from 'bootstrap';
+export default {
+    data() {
+        return {
+            appointment: false,
+        };
+    },
+    methods: {
+        showAppointmentModal() {
+            if (this.appointmentModalInstance) {
+                this.appointmentModalInstance.show();
+            } else {
+                console.error('Modal instance is not initialized.');
+            }
+        },
+        hideAppointmentModal() {
+            this.appointmentModalInstance.hide();
+        }
+    },
+    mounted() {
+        // Load Calendly widget script asynchronously
+        const script = document.createElement('script');
+        script.src = 'https://assets.calendly.com/assets/external/widget.js';
+        script.async = true;
+        // Append the script to the document body
+        document.body.appendChild(script);
+        this.appointmentModalInstance = new Modal(this.$refs.appointment, { backdrop: 'static' });
+    },
+};
+</script>
+
 <style>
-.personal-form-wrapper form{
+.personal-form-wrapper form {
     width: 50%;
+}
+
+#appointmentModal .modal-dialog {
+    max-width: 30%;
 }
 </style>
