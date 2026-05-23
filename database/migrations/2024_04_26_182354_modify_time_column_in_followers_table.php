6614c07a5dccf674777c878fef5752b791a014f3
Muhammad Saad Tariq <159819654+saadtariq57@users.noreply.github.com>
2026-05-23 20:01:15 +0500

    modified a migration

--- database/migrations/2024_04_26_182354_modify_time_column_in_followers_table.php
+++ database/migrations/2024_04_26_182354_modify_time_column_in_followers_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('followers', function (Blueprint $table) {
-            $table->timestamp('time')->nullable()->change();
+            $table->dropColumn('time');
+        });
+        
+        Schema::table('followers', function (Blueprint $table) {
+            $table->timestamp('time')->nullable();
         });
     }
 
